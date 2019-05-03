2f7404d688a3545701273d9fec3f77f387aec7fd
Fuad Aghazada <fuad.aghazada98@gmail.com>
2019-05-04 00:53:43 +0300

    Property Controller is added

---
+++ src/main/java/com/interdepartmental/service/PropertyService.java
@@ -0,0 +1,9 @@
+package com.interdepartmental.service;
+
+import com.interdepartmental.model.Property;
+
+import java.util.ArrayList;
+
+public interface PropertyService {
+    ArrayList<Property> get();
+}
